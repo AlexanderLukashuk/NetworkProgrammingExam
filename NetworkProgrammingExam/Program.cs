f2160c11e5100b858b249cfb6a08a135d644af95
AlexanderLukashuk <upiter465@gmail.com>
2020-11-20 20:38:27 +0600

    Добален пункт меню создания поста и отправки его на сервер

--- NetworkProgrammingExam/Program.cs
+++ NetworkProgrammingExam/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -9,12 +10,15 @@ namespace NetworkProgrammingExam
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string input = String.Empty;
             int menu;
             int postNumber = 1;
-            string pathToGetPosts = "https://jsonplaceholder.typicode.com/posts/";
+            string pathGET = "https://jsonplaceholder.typicode.com/posts/";
+            string pathPOST = "https://jsonplaceholder.typicode.com/posts/";
+            string postTitle;
+            string postBody;
             Console.WriteLine("Выберите опцию:");
             Console.WriteLine("1) Создать пост");
             Console.WriteLine("2) Получить список постов");
@@ -30,13 +34,50 @@ namespace NetworkProgrammingExam
                 {
                     case 1:
                         {
+                            var postRequest = WebRequest.Create(pathPOST);
+                            postRequest.Method = "POST";
 
+                            Console.Write("Введите заголовок: ");
+                            postTitle = Console.ReadLine();
+                            Console.Write("Введите текст поста: ");
+                            postBody = Console.ReadLine();
+
+                            Post post = new Post()
+                            {
+                                UserId = new Random().Next(1000),
+                                PostId = new Random().Next(1000),
+                                Title = postTitle,
+                                Body = postBody
+                            };
+
+                            string json = JsonSerializer.Serialize<Post>(post);
+
+                            byte[] buffer = Encoding.UTF8.GetBytes(json);
+
+                            postRequest.ContentType = "application/x-www-form-urlencoded";
+                            postRequest.ContentLength = buffer.Length;
+
+                            using (var stream = postRequest.GetRequestStream())
+                            {
+                                stream.Write(buffer, 0, buffer.Length);
+                            }
+
+                            //var postResponse = await postRequest.GetResponseAsync();
+                            //using (var stream = postResponse.GetResponseStream())
+                            //{
+                            //    using (var reader = new StreamReader(stream))
+                            //    {
+                            //        Console.WriteLine(reader.ReadToEnd());
+                            //    }
+                            //}
+
+                            //postResponse.Close();
 
                             break;
                         }
                     case 2:
                         {
-                            var getRequest = WebRequest.Create(pathToGetPosts);
+                            var getRequest = WebRequest.Create(pathGET);
                             getRequest.Method = "GET";
 
                             var getResponse = getRequest.GetResponse() as HttpWebResponse;
@@ -55,30 +96,30 @@ namespace NetworkProgrammingExam
                             input = Console.ReadLine();
                             postNumber = int.Parse(input);
 
-                            var getRequest = WebRequest.Create($"{pathToGetPosts}{postNumber}/");
+                            var getRequest = WebRequest.Create($"{pathGET}{postNumber}/");
                             getRequest.Method = "GET";
 
                             var getResponse = getRequest.GetResponse() as HttpWebResponse;
 
                             //Task.Run(() =>
                             //{
-                                using (var stream = new StreamReader(getResponse.GetResponseStream()))
-                                {
-                                    //Console.WriteLine(stream.ReadToEnd());
-                                    var json = stream.ReadToEnd();
-                                    //MessageBox.Show(json);
-                                    //client.DownloadFile(path, "file");
-                                    Post post = JsonSerializer.Deserialize<Post>(json);
+                            using (var stream = new StreamReader(getResponse.GetResponseStream()))
+                            {
+                                //Console.WriteLine(stream.ReadToEnd());
+                                var json = stream.ReadToEnd();
+                                //MessageBox.Show(json);
+                                //client.DownloadFile(path, "file");
+                                Post post = JsonSerializer.Deserialize<Post>(json);
 
-                                    Console.WriteLine($"{post.UserId}");
-                                    Console.WriteLine($"{post.PostId}");
-                                    Console.WriteLine($"{post.Title}");
-                                    Console.WriteLine($"{post.Body}");
-                                    posts.Add(post);
-                                    //context.Characters.Add(character);
-                                    //context.SaveChanges();
-                                }
-                                getResponse.Close();
+                                Console.WriteLine($"{post.UserId}");
+                                Console.WriteLine($"{post.PostId}");
+                                Console.WriteLine($"{post.Title}");
+                                Console.WriteLine($"{post.Body}");
+                                posts.Add(post);
+                                //context.Characters.Add(character);
+                                //context.SaveChanges();
+                            }
+                            getResponse.Close();
                             //});
 
                             break;
