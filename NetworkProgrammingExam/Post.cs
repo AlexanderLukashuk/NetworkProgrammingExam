20f2aa6b5fdb0a9e4fb311915e76874a82295527
AlexanderLukashuk <upiter465@gmail.com>
2020-11-20 20:14:22 +0600

    Добавлен класс Post, в котором есть свойсва о посте получаемом из json-файла. Добавлено меню(опции просмотра всех постов и получения информации об одном посте по номеру, который вводит пользователь)

---
+++ NetworkProgrammingExam/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace NetworkProgrammingExam
+{
+    public class Post
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+        [JsonProperty("id")]
+        public int PostId { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
--- NetworkProgrammingExam/Program.cs
+++ NetworkProgrammingExam/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace NetworkProgrammingExam
 {
@@ -6,7 +11,96 @@ namespace NetworkProgrammingExam
     {
         static void Main(string[] args)
         {
+            string input = String.Empty;
+            int menu;
+            int postNumber = 1;
+            string pathToGetPosts = "https://jsonplaceholder.typicode.com/posts/";
+            Console.WriteLine("Выберите опцию:");
+            Console.WriteLine("1) Создать пост");
+            Console.WriteLine("2) Получить список постов");
+            Console.WriteLine("3) Получить пост по номеру");
+            Console.WriteLine("4) Выход");
+            input = Console.ReadLine();
+            menu = int.Parse(input);
+            List<Post> posts = new List<Post>();
 
+            while (menu != 4)
+            {
+                switch (menu)
+                {
+                    case 1:
+                        {
+
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            var getRequest = WebRequest.Create(pathToGetPosts);
+                            getRequest.Method = "GET";
+
+                            var getResponse = getRequest.GetResponse() as HttpWebResponse;
+
+                            using (var stream = new StreamReader(getResponse.GetResponseStream()))
+                            {
+                                Console.WriteLine(stream.ReadToEnd());
+                            }
+                            getResponse.Close();
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Write("Введите номер поста: ");
+                            input = Console.ReadLine();
+                            postNumber = int.Parse(input);
+
+                            var getRequest = WebRequest.Create($"{pathToGetPosts}{postNumber}/");
+                            getRequest.Method = "GET";
+
+                            var getResponse = getRequest.GetResponse() as HttpWebResponse;
+
+                            //Task.Run(() =>
+                            //{
+                                using (var stream = new StreamReader(getResponse.GetResponseStream()))
+                                {
+                                    //Console.WriteLine(stream.ReadToEnd());
+                                    var json = stream.ReadToEnd();
+                                    //MessageBox.Show(json);
+                                    //client.DownloadFile(path, "file");
+                                    Post post = JsonSerializer.Deserialize<Post>(json);
+
+                                    Console.WriteLine($"{post.UserId}");
+                                    Console.WriteLine($"{post.PostId}");
+                                    Console.WriteLine($"{post.Title}");
+                                    Console.WriteLine($"{post.Body}");
+                                    posts.Add(post);
+                                    //context.Characters.Add(character);
+                                    //context.SaveChanges();
+                                }
+                                getResponse.Close();
+                            //});
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+
+                Console.WriteLine("Выберите опцию:");
+                Console.WriteLine("1) Создать пост");
+                Console.WriteLine("2) Получить список постов");
+                Console.WriteLine("3) Получить пост по номеру");
+                Console.WriteLine("4) Выход");
+                input = Console.ReadLine();
+                menu = int.Parse(input);
+            }
         }
     }
 }
